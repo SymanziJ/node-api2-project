577704381d03b1f5c532d3ec9e40ba7385187cf1
SymanziJ <97126299+SymanziJ@users.noreply.github.com>
2022-07-12 19:36:07 -0400

    server router files. router get post endpoints

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,36 @@
 // implement your posts router here
+const express = require('express');
+
+const Post = require('./posts-model');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    Post.find()
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ message: 'Error retrieving the posts'})
+        })
+})
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    Post.findById(id)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post);
+                return;
+            }            
+            res.status(404).json({ message: 'Post does not exist'});
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ message: 'Error retrieving the post'});
+        })
+})
+
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,18 @@
 // implement your server here
 // require your posts router and connect it here
+
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const postsRouter = require('./posts/posts-router');
+
+//const Post = require('./posts/posts-model');
+
+server.use('/api/posts', postsRouter);
+
+
+
+module.exports = server;
