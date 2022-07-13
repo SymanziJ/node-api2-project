c01daa19320727593907511a90b2b3c80c0a7ef9
SymanziJ <97126299+SymanziJ@users.noreply.github.com>
2022-07-12 23:56:31 -0400

    delete post, and get comments

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -12,7 +12,10 @@ router.get('/', (req, res) => {
         })
         .catch(err => {
             console.log(err);
-            res.status(500).json({ message: 'Error retrieving the posts'})
+            res.status(500).json({
+                message: 'The post information could not be retrieved',
+                err: err.message
+            });
         })
 })
 
@@ -24,11 +27,14 @@ router.get('/:id', (req, res) => {
                 res.status(200).json(post);
                 return;
             }            
-            res.status(404).json({ message: 'Post does not exist'});
+            res.status(404).json({ message: 'The post with the specified ID does not exist'});
         })
         .catch(err => {
             console.log(err);
-            res.status(500).json({ message: 'Error retrieving the post'});
+            res.status(500).json({
+                message: 'The post information could not be retrieved',
+                err: err.message
+            });
         })
 })
 
@@ -48,11 +54,90 @@ router.post('/', (req, res) => {
         })
         .catch(err => {
             console.log(err);
-            res.status(500).json({ message: 'Error creating the post'});
+            res.status(500).json({
+                message: 'There was an error while saving the post to the database',
+                err: err.message
+            });
         })
     }
     
 })
 
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const { contents, title } = req.body;
+    if (!title || !contents) {
+        res.status(400).json({
+            message: 'Please provide title and contents for the post'
+        })
+    } else {
+        Post.findById(req.params.id)
+            .then(stuff => {
+                if (!stuff) {
+                    res.status(404).json({
+                        message: 'The post with the specified ID does not exist'
+                    })
+                } else {
+                    return Post.update(req.params.id, req.body)
+                }
+            })
+            .then(data => {
+                if (data) {
+                    return Post.findById(id);
+                }
+            })
+            .then(post => {
+                if (post) {
+                    res.json(post);
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                    message: 'The post information could not be retrieved',
+                    err: err.message
+                });
+            })
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post) {
+            res.status(404).json({
+                message: 'The post with the specified ID does not exist'
+            })
+        } else {
+            await Post.remove(req.params.id)
+            res.json(post)
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'The post could not be removed',
+            err: err.message
+        });
+    }
+})
+
+router.get('/:id/comments', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            res.status(404).json({
+                message: 'The post with the specified ID does not exist'
+            })
+        } else {
+            const messages = await Post.findPostComments(req.params.id)
+            res.json(messages)
+        }
+    } catch (err) {
+        res.status(500).json({
+            messages: "The comments information could not be retrieved",
+            err: err.message
+        })
+    }
+})
 
 module.exports = router;
